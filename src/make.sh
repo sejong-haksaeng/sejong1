58810517669107be05472f5b5496005c78867a7e
자비에 <>
2016-02-13 23:42:01 +0100

    Switch font to Noto Sans.

--- src/make.sh
+++ src/make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./makeentries.py "../Sejong_1.txt" 
-latexmk -pdf vocab-list.tex
+xelatex -pdf vocab-list.tex
 mv vocab-list.pdf ..
-latexmk -pdf flashcard.tex
+xelatex flashcard.tex
 mv flashcard.pdf ..
